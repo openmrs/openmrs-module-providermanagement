f56cb4297bc08fa7e07ceedeb0a4bad0874f14c7
Mark Goodrich <mgoodrich@pih.org>
2012-04-16 16:25:08 -0400

    sync test, work in progress

--- api/src/test/java/org/openmrs/module/providermanagement/sync/SyncTest.java
+++ api/src/test/java/org/openmrs/module/providermanagement/sync/SyncTest.java
@@ -14,14 +14,22 @@
 
 package org.openmrs.module.providermanagement.sync;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.EncounterType;
+import org.openmrs.GlobalProperty;
 import org.openmrs.api.AdministrationService;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.ModuleUtil;
+import org.openmrs.module.providermanagement.ProviderRole;
+import org.openmrs.module.providermanagement.api.ProviderManagementService;
 import org.openmrs.module.sync.SyncBaseTest;
 import org.openmrs.module.sync.SyncTestHelper;
-import org.springframework.test.context.ContextConfiguration;
+import org.openmrs.test.StartModule;
+import org.openmrs.test.StartModuleExecutionListener;
+import org.springframework.test.annotation.NotTransactional;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -29,36 +37,59 @@ import static org.junit.Assert.assertNull;
 /**
  * Tests that the new OpenMRS objects added by the module sync properly
  */
+@StartModule(value = "sync-omod-1.1-SNAPSHOT.omod")
 public class SyncTest extends SyncBaseTest {
+
+    @Before
+    public void setGlobalProperty()  {
+        GlobalProperty gp = new GlobalProperty();
+
+        gp.setProperty("sync.database_version");
+        gp.setValue("11");
+
+        Context.getAdministrationService().saveGlobalProperty(gp);
+
+    }
 
     @Override
     public String getInitialDataset() {
-        return "org/openmrs/module/sync/include/SyncCreateTest.xml";
+        return "org/openmrs/module/providermanagement/include/providerManagement-dataset.xml";
     }
 
     @Test
+    @NotTransactional            // needed for some reason
     public void shouldUpdateEncounterType() throws Exception {
         runSyncTest(new SyncTestHelper() {
-            AdministrationService adminService = Context.getAdministrationService();
-            EncounterService encounterService = Context.getEncounterService();
-            public void runOnChild() {
 
+            EncounterService encounterService = Context.getEncounterService();
+
+            public void runOnChild() {
+                assertNotNull(Context.getAuthenticatedUser());
                 EncounterType encounterType = new EncounterType();
                 encounterType.setName("name");
                 encounterType.setDescription("description");
-                adminService.createEncounterType(encounterType);
-
-                EncounterType updateEncounterType = encounterService.getEncounterType("name");
-                encounterType.setName("new name");
-                adminService.updateEncounterType(updateEncounterType);
+                encounterService.saveEncounterType(encounterType);
             }
             public void runOnParent() {
+                assertNotNull(Context.getAuthenticatedUser());
                 EncounterType encounterType = encounterService.getEncounterType("name");
-                assertNull(encounterType);
-
-                encounterType = encounterService.getEncounterType("new name");
                 assertNotNull(encounterType);
             }
+
+
+            //ProviderManagementService providerManagementService = Context.getService(ProviderManagementService.class);
+
+            //public void runOnChild() {
+
+               // ProviderRole role = providerManagementService.getProviderRole(1001);
+                //role.setName("new name");
+                //providerManagementService.saveProviderRole(role);
+
+            //}
+            //public void runOnParent() {
+              //  ProviderRole role = providerManagementService.getProviderRole(1001);
+             //   Assert.assertEquals("new name", role.getName());
+            //}
         });
     }
 
