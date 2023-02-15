e0182c314ede3b259de41cf9cdf0d6d9d797b348
josephbate <sjb.lav@gmail.com>
2023-02-15 10:42:56 +0300

    rremoving ReflectString.java

--- api/src/main/java/org/openmrs/module/providermanagement/ReflectString.java
+++
@@ -1,9 +0,0 @@
-package org.openmrs.module.providermanagement;
-
-public class ReflectString {
-	
-	public static String sMethod() {
-		   return "The String is returned by using Return Statement";
-		}
-
-}
